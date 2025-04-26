009353efb0790a3cab6c3c69cb480eac376993c5
alejandra-andrade <alejandra.andradegarcia@galileo.edu>
2025-04-25 22:45:53 -0600

    Actividad 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import './App.scss';
+import Item from './components/item/item'; 
+import Menu from './components/menu/menu';
+import Formulario from './components/formulario/formulario';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Menu />
+      <Container>
+        <Row>
+        <Col>
+          <Formulario />
+        </Col>
+        <Col>
+          <Item />
+          <Item /> 
+          <Item /> 
+        </Col>
+        </Row>
+      </Container>
+
     </div>
   );
 }
